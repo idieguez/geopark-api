85a948d445a39db98a06f84e760a5835b8d339ec
Israel Diéguez <8616829+idieguez@users.noreply.github.com>
2025-02-23 18:17:26 +0100

    Configuramos CORS.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const authRoutes = require('./routes/auth-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -10,13 +11,21 @@ const vehiclesRoutes = require('./routes/vehicles-routes');
 // Create the Express application.
 const app = express();
 
+// Configure CORS.
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 // Configure and use the rate limiter.
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,                                       // 15 minutes.
     max: 100,                                                       // Limit to 100 requests per IP every 15 minutes.
     message: `Too many requests from this IP, try again later.`,
-    standardHeaders: true,                                          // Informa límites de tasa con cabeceras estándar
-    legacyHeaders: false,                                           // Desactiva cabeceras obsoletas
+    standardHeaders: true,                                          // Reports rate limits with standard headers.
+    legacyHeaders: false,                                           // Disable obsolete headers.
 });
 app.use(limiter);
 
