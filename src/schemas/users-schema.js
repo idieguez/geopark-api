4f9f5d2b4b8840fe83d8ad4d7c0ee23253c61fca
Israel Diéguez <8616829+idieguez@users.noreply.github.com>
2025-11-06 21:30:19 +0100

    Mejora de los comentarios.

--- src/schemas/users-schema.js
+++ src/schemas/users-schema.js
@@ -11,18 +11,21 @@ exports.updateUserSchema = z.object({
 
     body: z.object({
 
+        // Name.
         name: z.string().trim()
             .min(1, 'Name is required')
             .max(50, 'Name cannot exceed 50 characters')
             .regex(/^[a-zA-ZÀ-ÿ\s]*$/, 'Name cannot contain numbers or special characters')
             .optional(),
         
+        // Surname.
         surname: z.string().trim()
             .min(1, 'Surname is required')
             .max(50, 'Surname cannot exceed 50 characters')
             .regex(/^[a-zA-ZÀ-ÿ\s]*$/, 'Surname cannot contain numbers or special characters')
             .optional(),
         
+        // Second surname.
         secondSurname: z.string().trim()
             .max(50, 'Second surname cannot exceed 50 characters')
             .regex(/^[a-zA-ZÀ-ÿ\s]*$/, 'Second surname cannot contain numbers or special characters')
@@ -31,6 +34,7 @@ exports.updateUserSchema = z.object({
             .optional()
             .default(null),
         
+        // Password.
         password: z.string()
             .min(8, 'Password must be at least 8 characters')
             .max(40, 'Password cannot exceed 40 characters')
@@ -40,12 +44,14 @@ exports.updateUserSchema = z.object({
             )
             .optional(),
         
+        // Newsletter.
         newsletter: z.boolean({
             required_error: 'Newsletter preference is required',
             invalid_type_error: 'Newsletter must be a boolean'
         })
         .optional(),
 
+        // Settings.
         settings: z.object({
             appearance: z.enum(['auto', 'light', 'dark'], {
                 invalid_type_error: 'Appearance must be one of: auto, light, dark',
@@ -54,6 +60,7 @@ exports.updateUserSchema = z.object({
         })
         .optional(),
         
+        // Notes.
         notes: z.string().trim()
             .max(500, 'Notes cannot exceed 500 characters')
             .transform(value => (value === '' ? null : value))
--- src/schemas/vehicles-schema.js
+++ src/schemas/vehicles-schema.js
@@ -11,25 +11,30 @@ exports.createVehicleSchema = z.object({
 
     body: z.object({
 
+        // License plate.
         licensePlate: z.string().trim()
             .min(7, 'License plate must have 7 characters')
             .max(7, 'License plate must have 7 characters')
             .regex(/^\d{4}[a-zA-Z]{3}$/, 'License plate must be 4 numbers followed by 3 letters with no spaces in between (e.g., 1234ABC)')
             .transform(s => s.toUpperCase()),
         
+        // Type.
         type: z.enum(['car', 'motorbike'], {
             required_error: 'Vehicle type is required',
             invalid_type_error: 'Vehicle type must be "car" or "motorbike"'
         }),
         
+        // Brand.
         brand: z.string().trim()
             .min(1, 'Vehicle brand is required')
             .max(50, 'Vehicle brand cannot exceed 50 characters'),
         
+        // Model.
         model: z.string().trim()
             .min(1, 'Vehicle model is required')
             .max(50, 'Vehicle model cannot exceed 50 characters'),
         
+        // Notes.
         notes: z.string().trim()
             .max(500, 'Notes cannot exceed 500 characters')
             .transform(value => (value === '' ? null : value))
@@ -52,6 +57,7 @@ exports.getVehicleSchema = z.object({
 
     params: z.object({
 
+        // License plate.
         licensePlate: z.string().trim()
             .min(7, 'License plate must have 7 characters')
             .max(7, 'License plate must have 7 characters')
@@ -74,6 +80,7 @@ exports.updateVehicleSchema = z.object({
     // Route params.
     params: z.object({
 
+        // License plate.
         licensePlate: z.string().trim()
             .min(7, 'License plate must have 7 characters')
             .max(7, 'License plate must have 7 characters')
@@ -85,21 +92,25 @@ exports.updateVehicleSchema = z.object({
     // Body.
     body: z.object({
 
+        // Type.
         type: z.enum(['car', 'motorbike'], {
             invalid_type_error: 'Vehicle type must be "car" or "motorbike"'
         })
         .optional(),
         
+        // Brand.
         brand: z.string().trim()
             .min(1, 'Vehicle brand cannot be empty')
             .max(50, 'Vehicle brand cannot exceed 50 characters')
             .optional(),
         
+        // Model.
         model: z.string().trim()
             .min(1, 'Vehicle model cannot be empty')
             .max(50, 'Vehicle model cannot exceed 50 characters')
             .optional(),
 
+        // Location.
         location: z.object({
             
             latitude: z.number({
@@ -118,6 +129,7 @@ exports.updateVehicleSchema = z.object({
         .strict()
         .optional(),
         
+        // Notes.
         notes: z.string().trim()
             .max(500, 'Notes cannot exceed 500 characters')
             .transform(value => (value === '' ? null : value))
@@ -140,6 +152,7 @@ exports.deleteVehicleSchema = z.object({
 
     params: z.object({
 
+        // License plate.
         licensePlate: z.string().trim()
             .min(7, 'License plate must have 7 characters')
             .max(7, 'License plate must have 7 characters')
