157dd369213d1cd27876e493932775e5d1338564
Israel Diéguez <8616829+idieguez@users.noreply.github.com>
2025-11-12 21:25:21 +0100

    Se realizan mejoras en materia de seguridad: control del número de intentos de login de un usuario y posterior bloqueo temporal, verificación de que el token JWT fue creado posterior al último cambio de contraseña o no permitimos el acceso. También se ha realizado algún pequeño ajuste.

--- src/models/User.js
+++ src/models/User.js
@@ -11,14 +11,16 @@ exports.User = mongoose.model('User', new mongoose.Schema({
 
     name: { type: String, required: true },
     surname: { type: String, required: true },
-    secondSurname: { type: String, default: '' },
+    secondSurname: { type: String, default: null },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Encrypted password
     newsletter: { type: Boolean, required: true, default: false },
     settings: {
         appearance: { type: String, enum: ['auto', 'light', 'dark'], required: true, default: 'auto' }
     },
-    notes: { type: String, default: '' },
+    notes: { type: String, default: null },
+    loginAttempts: { type: Number, required: true, default: 0 },
+    lockUntil: { type: Date, default: null },
     dateUserCreation: { type: Date, required: true, default: Date.now },
     dateLastUserModification: { type: Date, required: true, default: Date.now },
     dateLastPasswordModification: { type: Date, required: true, default: Date.now }
